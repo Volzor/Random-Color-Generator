32f9c731ba504a1dcd3e273bd6e8a87f700e42fb
Volzor <96282373+Volzor@users.noreply.github.com>
2022-11-04 12:35:17 -0700

    added js
    added random color generator function and added event listener to button

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const button = document.getElementById('submit-btn');
 const container = document.querySelector('.container');
+const colorArray = ['rgb(0, 0, 153)', 'rgb(51, 255, 51)', 'rgb(255, 0, 0)', 'rgb(153, 153, 0)', 'rgb(153, 0, 153)'];
+
+const randomGenerator = () => {
+    const randomIndex = Math.floor(Math.random() * colorArray.length)
+    container.style.backgroundColor = colorArray[randomIndex]
+}
+
+button.addEventListener('click', randomGenerator)
